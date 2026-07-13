a43b470191a469eabb921f332bc6dbb387fbecc9
egraich <egorbabchuk1@gmail.com>
2026-07-13 19:37:02 +0300

    feat: CONSSTANTS

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+import pygame
 import numpy as np
 
+WIDTH, HEIGHT = 800, 600
+CELL_SIZE = 8  # Размер одной клетки в пикселях
+COLS, ROWS = WIDTH // CELL_SIZE, HEIGHT // CELL_SIZE
 
+COLOR_BG = (10, 10, 15)       # Тёмно-синий космос
+COLOR_GRID = (20, 20, 30)     # Едва заметная сетка
+COLOR_DIE = (40, 40, 50)      # Цвет следа (можно убрать)
+COLOR_ALIVE = (0, 255, 150)   # Ядовито-зелёные живые клетки
 
-
-
-
-
-
-
-
-grid = np.random.choice([0, 1], size=(50, 50), p=[0.8, 0.2]).astype(np.int8)
-print(type(grid))
